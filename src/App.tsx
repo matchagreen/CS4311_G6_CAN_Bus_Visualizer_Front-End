795c67c1d6dd84fb05c8a2558a69fd19961f1c9a
Alan Perez <alanperezse@gmail.com>
2022-09-16 12:41:50 -0600

    Created project file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './entrypoint/index';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Intro from './entrypoint/index'
+import Projects from './projects/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Intro/>}/>
+          <Route path='projects' element={<Projects/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/projects/index.tsx
@@ -0,0 +1,9 @@
+import './index.css';
+
+function Projects() {
+  return (
+    <h1>Projects Page</h1>
+  );
+}
+
+export default Projects;
