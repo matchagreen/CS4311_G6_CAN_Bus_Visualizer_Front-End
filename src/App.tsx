00ed480bf281d4999b2f5688e38f02fea9baa9c2
Alan Perez <alanperezse@gmail.com>
2022-09-17 10:19:56 -0600

    Added routing for new-projects page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './entrypoint/index';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Intro from './entrypoint/index'
 import Projects from './projects/index';
+import NewProject from './projects/new';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Intro/>}/>
           <Route path='projects' element={<Projects/>}/>
+          <Route path='projects/new' element={<NewProject/>}/>
         </Routes>
       </div>
     </Router>
