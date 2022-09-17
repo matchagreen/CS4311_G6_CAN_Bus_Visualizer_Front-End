f39024cf8febf95b2885fa72ec3b2243fe753b8e
Alan Perez <alanperezse@gmail.com>
2022-09-17 11:17:12 -0600

    Added bootstrap styling to app

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Intro from './entrypoint/index'
 import Projects from './projects/index';
 import NewProject from './projects/new';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
