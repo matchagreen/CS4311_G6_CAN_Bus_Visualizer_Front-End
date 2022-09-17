503924c434409159fb01f125254bca0277823324
Alan Perez <alanperezse@gmail.com>
2022-09-17 10:20:37 -0600

    Added files for new feature

---
+++ src/projects/new/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+class NewProject extends React.Component {
+  render() {
+    return (
+      <h1>New Project</h1>
+    );
+  }
+}
+
+export default NewProject
