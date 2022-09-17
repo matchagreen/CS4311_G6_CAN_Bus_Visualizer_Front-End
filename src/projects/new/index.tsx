fa90e2f03c6f5d27ff9df7e58f93f3afacb927f0
Alan Perez <alanperezse@gmail.com>
2022-09-17 14:02:36 -0600

    Added page and styling. Missing logic to get files and to call API

--- src/projects/new/index.tsx
+++ src/projects/new/index.tsx
@@ -1,12 +1,62 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import React from 'react';
 import './index.css';
+import { Link } from 'react-router-dom';
 
 class NewProject extends React.Component {
-  render() {
-    return (
-      <h1>New Project</h1>
-    );
-  }
+    state = {
+        projectName: '',
+        baudRate: 9600,
+        userInitials: '',
+    }
+
+    render() {
+        return (
+            <div className='new-project'>
+                <h1 className='new-project-title'>New Project</h1>
+                <div className='new-project-form'>
+                    <Form>
+                        <div className='new-project-fields'>
+                            <Form.Group className='mb-3' controlId='formGroupText'>
+                                <Form.Label>Project name</Form.Label>
+                                <Form.Control
+                                    type='text'
+                                    name='projectName'
+                                    value={this.state.projectName}
+                                    onChange={e => this.setState({projectName: e.target.value})}
+                                    placeholder='PBJ'
+                                />
+                            </Form.Group>
+                            <Form.Group className='mb-3' controlId='formGroupNumber'>
+                                <Form.Label>Baud-rate</Form.Label>
+                                <Form.Control
+                                    type='number'
+                                    name='baudRate'
+                                    value={this.state.baudRate}
+                                    onChange={e => this.setState({baudRate: e.target.value})}
+                                    placeholder='9600'
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="formFile" className="mb-3">
+                                <Form.Label>DBC file</Form.Label>
+                                <Form.Control type="file" />
+                            </Form.Group>
+                            <Form.Group controlId="formFile" className="mb-3">
+                                <Form.Label>Blacklist</Form.Label>
+                                <Form.Control type="file" />
+                            </Form.Group>
+                        </div>
+                        <div className='new-project-buttons'>
+                            <Button>Cancel</Button>
+                            <div className='space'></div>
+                            <Button type='submit'>Done</Button>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default NewProject
