a7306d83fa4c4f33dba06099a2e1d1db075edc21
Alan Perez <alanperezse@gmail.com>
2022-09-18 09:57:21 -0600

    Added methods for fetching projects and creating new projects

--- src/utilities/APIutils.tsx
+++ src/utilities/APIutils.tsx
@@ -1,5 +1,17 @@
 class APIUtil {
+    url = 'http://localhost:8000'
 
+    getProjectList() {
+        fetch(this.url + '/projects')
+            .then((response) => response.json)
+            .then((responseJson) => {return responseJson})
+    }
+
+    createProject() {
+        fetch(this.url + '/projects/new', {method: 'POST'})
+            .then((response) => response.json)
+            .then((responseJson) => {return responseJson})
+    }
 }
 
 export default APIUtil
