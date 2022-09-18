a339172cb978f4bd4d1c06b2c10ae86d014995c6
Mauricio Garcia <magarciatov@miners.utep.edu>
2022-09-18 00:39:23 -0600

    create landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './entrypoint/index';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Intro from './entrypoint/index'
 import Projects from './projects/index';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
