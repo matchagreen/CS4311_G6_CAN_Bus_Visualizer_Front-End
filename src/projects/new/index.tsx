d93964b5c64cf533bf76f2bc8c80fdc05ca9c1d3
Alan Perez <alanperezse@gmail.com>
2022-09-17 18:55:21 -0600

    Transformed class into function and added redirection on cancel

--- src/projects/new/index.tsx
+++ src/projects/new/index.tsx
@@ -1,71 +1,67 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import React from 'react';
+import {useNavigate} from "react-router-dom";
+import {useState} from 'react';
 import './index.css';
 
-class NewProject extends React.Component {
-    state = {
+function NewProject() {
+    const [state, setState] = useState({
         projectName: '',
         baudRate: 9600,
         analystInitials: '',
+    })
+
+    let navigate = useNavigate();
+
+    const onCancel = ()=> {
+        const  path = './..'
+        navigate(path)
     }
 
-    render() {
-        return (
-            <div className='new-project'>
-                <h1 className='new-project-title'>New Project</h1>
-                <div className='new-project-form'>
-                    <Form>
-                        <div className='new-project-fields'>
-                            <Form.Group className='mb-3' controlId='formGroupText'>
-                                <Form.Label>Project name</Form.Label>
-                                <Form.Control
-                                    type='text'
-                                    name='projectName'
-                                    value={this.state.projectName}
-                                    onChange={e => this.setState({projectName: e.target.value})}
-                                    placeholder='PBJ'
-                                />
-                            </Form.Group>
-                            <Form.Group className='mb-3' controlId='formGroupText'>
-                                <Form.Label>Analyst initials</Form.Label>
-                                <Form.Control
-                                    type='text'
-                                    name='analystInitials'
-                                    value={this.state.analystInitials}
-                                    onChange={e => this.setState({analystInitials: e.target.value})}
-                                    placeholder='PBJ'
-                                />
-                            </Form.Group>
-                            <Form.Group className='mb-3' controlId='formGroupNumber'>
-                                <Form.Label>Baud-rate</Form.Label>
-                                <Form.Control
-                                    type='number'
-                                    name='baudRate'
-                                    value={this.state.baudRate}
-                                    onChange={e => this.setState({baudRate: e.target.value})}
-                                    placeholder='9600'
-                                />
-                            </Form.Group>
-                            <Form.Group controlId="formFile" className="mb-3">
-                                <Form.Label>DBC file</Form.Label>
-                                <Form.Control type="file" />
-                            </Form.Group>
-                            <Form.Group controlId="formFile" className="mb-3">
-                                <Form.Label>Blacklist</Form.Label>
-                                <Form.Control type="file" />
-                            </Form.Group>
-                        </div>
-                        <div className='new-project-buttons'>
-                            <Button>Cancel</Button>
-                            <div className='space'></div>
-                            <Button type='submit'>Done</Button>
-                        </div>
-                    </Form>
-                </div>
+    return (
+        <div className='new-project'>
+            <h1 className='new-project-title'>New Project</h1>
+            <div className='new-project-form'>
+                <Form>
+                    <div className='new-project-fields'>
+                        <Form.Group className='mb-3' controlId='formGroupText'>
+                            <Form.Label>Project name</Form.Label>
+                            <Form.Control
+                                type='text'
+                                name='projectName'
+                                value={state.projectName}
+                                onChange={(e)=>setState({...state, projectName: e.target.value})}
+                                placeholder='PBJ'
+                            />
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId='formGroupText'>
+                            <Form.Label>Analyst initials</Form.Label>
+                            <Form.Control
+                                type='text'
+                                name='analystInitials'
+                                value={state.analystInitials}
+                                onChange={e=>setState({...state, analystInitials: e.target.value})}
+                                placeholder='PBJ'
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Label>DBC file</Form.Label>
+                            <Form.Control type="file" />
+                        </Form.Group>
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Label>Blacklist</Form.Label>
+                            <Form.Control type="file" />
+                        </Form.Group>
+                    </div>
+                    <div className='new-project-buttons'>
+                        <Button onClick={onCancel}>Cancel</Button>
+                        <div className='space'></div>
+                        <Button type='submit'>Done</Button>
+                    </div>
+                </Form>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default NewProject
