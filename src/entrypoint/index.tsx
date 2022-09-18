644972f24fde58036b4204a3e9b30af99e64dde7
Mauricio Garcia <magarciatov@miners.utep.edu>
2022-09-18 00:18:09 -0600

    create landing page

--- src/entrypoint/index.tsx
+++ src/entrypoint/index.tsx
@@ -1,9 +1,41 @@
+import Button from 'react-bootstrap/Button';
+
+import {useNavigate} from "react-router-dom";
 import './index.css';
 
-function Intro() {
-  return (
-    <h1>Hello world</h1>
-  );
+function NewProject() {
+    
+
+    let navigate = useNavigate();
+
+    const onProjects = ()=> {
+        const  path = './projects'
+        navigate(path)
+    }
+
+    const onSync = ()=> {
+        const  path = './sync'
+        navigate(path)
+    }
+
+    const onCANBusVisualizer = ()=> {
+        const  path = './CANBusVisualizer'
+        navigate(path)
+    }
+
+    return (
+        <div className='task'>
+            <h1 className='task-title'>CAN Bus Visualizer</h1>
+            
+            <div className='task-buttons'>
+                <Button onClick={onProjects}>Projects</Button>
+                <div className='space'></div>
+                <Button onClick={onSync}>Sync</Button>
+                <div className='space'></div>
+                <Button onClick={onCANBusVisualizer}>CAN Bus Visualizer</Button>
+            </div>
+        </div>
+    )
 }
 
-export default Intro;
+export default NewProject
