0ff01943d39594dd404f0f78d785c3352c382298
Mauricio Garcia <81186505+garciagitmauricio@users.noreply.github.com>
2022-09-18 18:38:02 -0600

    Update index.tsx

--- src/entrypoint/index.tsx
+++ src/entrypoint/index.tsx
@@ -1,41 +1,9 @@
-import Button from 'react-bootstrap/Button';
-
-import {useNavigate} from "react-router-dom";
 import './index.css';
 
-function NewProject() {
-    
-
-    let navigate = useNavigate();
-
-    const onProjects = ()=> {
-        const  path = './projects'
-        navigate(path)
-    }
-
-    const onSync = ()=> {
-        const  path = './sync'
-        navigate(path)
-    }
-
-    const onCANBusVisualizer = ()=> {
-        const  path = './CANBusVisualizer'
-        navigate(path)
-    }
-
-    return (
-        <div className='task'>
-            <h1 className='task-title'>CAN Bus Visualizer</h1>
-            
-            <div className='task-buttons'>
-                <Button onClick={onProjects}>Projects</Button>
-                <div className='space'></div>
-                <Button onClick={onSync}>Sync</Button>
-                <div className='space'></div>
-                <Button onClick={onCANBusVisualizer}>CAN Bus Visualizer</Button>
-            </div>
-        </div>
-    )
+function Intro() {
+  return (
+    <h1>Hello world</h1>
+  );
 }
 
-export default NewProject
+export default Intro;
