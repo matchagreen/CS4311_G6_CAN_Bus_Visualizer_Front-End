c974d48e50427c255fe6ae4d7987c9dcebc24e2e
Mauricio Garcia <magarciatov@miners.utep.edu>
2022-09-18 20:21:55 -0600

    add icons to buttons of landing page

--- src/entrypoint/index.tsx
+++ src/entrypoint/index.tsx
@@ -1,7 +1,11 @@
 import Button from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 import {useNavigate} from "react-router-dom";
 import './index.css';
+
+
 
 function NewProject() {
     
@@ -28,11 +32,30 @@ function NewProject() {
             <h1 className='task-title'>CAN Bus Visualizer</h1>
             
             <div className='task-buttons'>
-                <Button onClick={onProjects}>Projects</Button>
+
+                <Button onClick={onProjects}>
+                <i style={{ paddingRight: "10px" }}
+                className="fa fa-folder"
+                aria-hidden="true"></i>
+                Projects
+                </Button>
+
                 <div className='space'></div>
-                <Button onClick={onSync}>Sync</Button>
+
+                <Button onClick={onSync}>
+                <i style={{ paddingRight: "10px" }}
+                className="fa fa-superpowers"
+                aria-hidden="true"></i>
+                Sync
+                </Button>
+
                 <div className='space'></div>
-                <Button onClick={onCANBusVisualizer}>CAN Bus Visualizer</Button>
+                <Button onClick={onCANBusVisualizer}>
+                <i style={{ paddingRight: "10px" }}
+                className="fa fa-car"
+                aria-hidden="true"></i>
+                CAN Bus Visualizer
+                </Button>
             </div>
         </div>
     )
