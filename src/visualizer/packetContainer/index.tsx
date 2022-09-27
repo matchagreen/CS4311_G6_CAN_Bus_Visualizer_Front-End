eaec392146fc5e83b17a4525c79af4a4caaaff14
Alan Perez <alanperezse@gmail.com>
2022-09-27 16:50:39 -0600

    Added visuals for packet list

--- src/visualizer/packetContainer/index.tsx
+++ src/visualizer/packetContainer/index.tsx
@@ -1,8 +1,21 @@
 import PacketList from "./PacketList"
+import { Button} from "react-bootstrap"
+import { ArrowRepeat } from "react-bootstrap-icons"
+
+import './index.css'
 
 function PacketContainer() {
     return (
-        <PacketList></PacketList>
+        <div className='packet-container rounded'>
+            <h3>Packets</h3>
+            <Button className='rounded-pill'>
+                Fetch &nbsp;
+                <ArrowRepeat/>
+            </Button>
+            <br />
+            <br />
+            <PacketList></PacketList>
+        </div>
     )
 }
 
