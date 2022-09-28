040c7ad46923a34c1994ec35a005b7ac517187b9
Mauricio Garcia <magarciatov@miners.utep.edu>
2022-09-28 12:28:13 -0600

    add navbar to projects

--- src/visualizer/index.tsx
+++ src/visualizer/index.tsx
@@ -1,9 +1,23 @@
 import {useParams} from 'react-router-dom'
+import Navbar from "../projecttemplate/components/Navbar";
+import '../projecttemplate/index.css';
+import React from 'react';
 
 function Visualizer() {
     const params = useParams()
     
-    return <h1>{params.projectId}</h1>
-}
+    return(
+        <header className="projectheader">
+        
+        <div className="nav-area">
+            <h1>{params.projectId}</h1>
+            
+            
+            <Navbar />
+        </div>
+        </header>
+        
+    );
+};
 
 export default Visualizer
