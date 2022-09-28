608111460bca86485eff60df33ad420a3a0de908
Mauricio Garcia <magarciatov@miners.utep.edu>
2022-09-28 12:59:44 -0600

    change menu bar style

--- src/projecttemplate/components/Dropdown.js
+++ src/projecttemplate/components/Dropdown.js
@@ -3,9 +3,9 @@ import React from 'react';
 import '../index.css';
 const Dropdown = ({ submenus, dropdown, depthLevel }) => {
   depthLevel = depthLevel + 1;
-  const dropdownClass = depthLevel > 1 ? "dropdown-submenu" : "";
+  const dropdownClass = depthLevel > 1 ? "drop-down-submenu" : "";
   return (
-    <ul className={`dropdown ${dropdownClass} ${dropdown ? "show" : ""}`}>
+    <ul className={`drop-down ${dropdownClass} ${dropdown ? "show" : ""}`}>
       {submenus.map((submenu, index) => (
         <MenuItems items={submenu} key={index} depthLevel={depthLevel} />
       ))}
