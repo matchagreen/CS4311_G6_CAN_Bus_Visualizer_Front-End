44794d4dcaa38461ed22c93dbfd8ea6731d87bc4
Alan Perez <alanperezse@gmail.com>
2022-09-27 22:12:46 -0600

    Switched br for padding

--- src/visualizer/packetContainer/index.tsx
+++ src/visualizer/packetContainer/index.tsx
@@ -14,7 +14,6 @@ function PacketContainer() {
                     <ArrowRepeat/>
                 </Button>
             </div>
-            <br />
             <PacketList></PacketList>
         </div>
     )
