a86467e06c86a2588a31d4f76008e87d222c8d68
Mauricio Garcia <81186505+garciagitmauricio@users.noreply.github.com>
2022-09-30 00:16:03 -0600

    Revert "Packet infinite list"

--- src/visualizer/index.tsx
+++ src/visualizer/index.tsx
@@ -1,16 +1,9 @@
 import {useParams} from 'react-router-dom'
-import PacketContainer from './packetContainer'
-import './index.css'
 
 function Visualizer() {
     const params = useParams()
     
-    return (
-        <div className='visualizer'>
-            <h1 className='visualizer-title'>{params.projectId}</h1>
-            <PacketContainer></PacketContainer>
-        </div>
-    )
+    return <h1>{params.projectId}</h1>
 }
 
 export default Visualizer
