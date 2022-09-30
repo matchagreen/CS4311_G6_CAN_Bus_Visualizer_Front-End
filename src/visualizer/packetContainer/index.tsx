1b03064e898ba0848a2bb17fcb0cc61830ac2b4c
Alan Perez <alanperezse@gmail.com>
2022-09-30 13:46:54 -0600

    Added play/stop button

--- src/visualizer/packetContainer/index.tsx
+++ src/visualizer/packetContainer/index.tsx
@@ -1,18 +1,40 @@
 import PacketList from "./PacketList"
 import { Button} from "react-bootstrap"
-import { ArrowRepeat } from "react-bootstrap-icons"
-
+import { ArrowRepeat, PauseFill, PlayFill } from "react-bootstrap-icons"
+import { useState } from "react";
 import './index.css'
 
 function PacketContainer() {
+    let [isPlaying, setIsPlaying] = useState(false);
+
     return (
         <div className='packet-container rounded'>
             <div className='packet-container-inner'>
                 <h6>Packets</h6>
-                <Button className='rounded-pill' size='sm'>
-                    Fetch &nbsp;
-                    <ArrowRepeat/>
-                </Button>
+                <div className='packet-buttons'>
+                    <Button className='rounded-pill' size='sm'>
+                        <ArrowRepeat/>
+                        &nbsp;
+                        Fetch
+                    </Button>
+                    &nbsp;
+                    {isPlaying ?
+                        (
+                            <Button className='rounded-pill' variant='success' size='sm' onClick={() => setIsPlaying(false)}>
+                                <PauseFill/>
+                                &nbsp;
+                                Stop
+                            </Button>
+                        ) : 
+                        (
+                            <Button className='rounded-pill' variant='light' size='sm' onClick={() => setIsPlaying(true)}>
+                                <PlayFill/>
+                                &nbsp;
+                                Play
+                            </Button>
+                        )
+                    }
+                </div>
             </div>
             <PacketList></PacketList>
         </div>
