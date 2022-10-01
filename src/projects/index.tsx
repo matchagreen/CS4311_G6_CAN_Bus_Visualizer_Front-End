f975afd017a1d5ae741a9a75e2104776f9cd499b
ianrigsbee54 <irrigsbee@miners.utep.edu>
2022-10-01 13:30:47 -0600

    initial page redesign

--- src/projects/index.tsx
+++ src/projects/index.tsx
@@ -2,7 +2,8 @@ import './index.css';
 import APIUtil from '../utilities/APIutils';
 import {useNavigate} from "react-router-dom";
 import ProjectCardState from './ProjectCardState';
-import { Button, ButtonGroup, Col, Container, Dropdown, Row } from 'react-bootstrap';
+import { Button, ButtonGroup, Col, Nav, Dropdown, Row, Tab, TabContainer, NavItem } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 function Projects() {
 
@@ -12,66 +13,71 @@ function Projects() {
         const  path = './new'
         navigate(path)
     }
-
-    const onArchives = ()=> {
-      const  path = './archives'
-      navigate(path)
-    }
-
     const api = new APIUtil()
 
     function getProjectCards(){
       let projects = api.getProjects()
         return projects.map((val: ProjectCardState) => {
           return  <Dropdown as={ButtonGroup} className='mock-project' >
-          <Button  className='inside-mock' variant="success">{val.name}</Button>
-          <Dropdown.Toggle className='inside-mock'split variant="success" id="dropdown-split-basic" />
+          <Button  className='inside-mock' variant="primary">{val.name}</Button>
+          <Dropdown.Toggle className='inside-mock'split variant="primary" id="dropdown-split-basic"/>
 
           <Dropdown.Menu>
             <Dropdown.Item href="#/action-1">Archive</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Duplicate</Dropdown.Item>
             <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
+          </Dropdown.Menu>
         </Dropdown>
         })
       }
 
     let projectCards = getProjectCards()
-  
   return (
-    <Container className='container'>
-      {/* Stack the columns on mobile by making one full-width and the other half-width */}
-      <header className='header'>
-        <Dropdown >
-          <Dropdown.Toggle className='dropdown' variant="success" id="dropdown-basic">
-            Projects
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={onArchives}>Archives</Dropdown.Item>
-          </Dropdown.Menu>
-
-        </Dropdown>
-      </header>
+    <Tab.Container id='projectTabs' defaultActiveKey='activeProjects'>
       <Row>
-
-          <Col className='col' xs={4} md={2}>
-            <div className='side-bar' > 
-              <Button className = "side-buttons" onClick={onNewProject}>New</Button>
-              <br></br>
-              <Button className = "side-buttons">
-  
-              </Button>
-            </div>
-          </Col>
-          <Col className='col' xs={14} md={10}>
-      
-            {projectCards}
-          </Col>
-        </Row>
-    </Container>
+        <Col sm={3} className='tabColumn'>
+          <Nav variant='pills' className='flex-column'>
+            <Nav.Item>
+              <Nav.Link eventKey='importProject'>
+                Import Project
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey='newProject' onClick={onNewProject}>
+                Create Project
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey='activeProjects'>
+                Active Projects
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey='archivedProjects'>
+                Archived Projects
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            <Tab.Pane eventKey='importProject'>
+              <h3 className='projectHeader3'>Import Project</h3>
+              Due to import project method not existing on backend this is empty
+            </Tab.Pane>
+            <Tab.Pane eventKey='activeProjects'>
+              <h3 className='projectHeader3'>Active Projects</h3>
+              {projectCards}
+            </Tab.Pane>
+            <Tab.Pane eventKey='archivedProjects'>
+              <h3 className='projectHeader3'>Archived Projects</h3>
+              {projectCards}
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
   );
 }
-
 
 export default Projects;
