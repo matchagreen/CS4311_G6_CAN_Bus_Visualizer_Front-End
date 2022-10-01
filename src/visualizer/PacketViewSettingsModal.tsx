858d470aa068cb77d31ac5a47a925d4758d97816
Alan Perez <alanperezse@gmail.com>
2022-10-01 15:21:29 -0600

    Added styling and closing function to modal

--- src/visualizer/PacketViewSettingsModal.tsx
+++ src/visualizer/PacketViewSettingsModal.tsx
@@ -1,43 +1,48 @@
 import { InputGroup, Row, Col, Modal, Form, Button} from 'react-bootstrap'
+import {PacketSortOptions as Sort} from '../common/Constants'
 
 function PacketViewSettingsModal({onHide, onSubmit, show}: any) {
     return (
-        <Modal show={show} onHide={onHide} className='test'>
+        <Modal show={show} onHide={onHide} className='packet-view-modal'>
             <Modal.Header closeButton>
                 <Modal.Title>Packet View Settings</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <Form onSubmit={onSubmit}>
-                    <h5>Order</h5>
-                    <InputGroup className="mb-3" size='sm'>
-                        <InputGroup.Text id="basic-addon1">Sort by:</InputGroup.Text>
-                        <Form.Select size='sm'>
-                            <option value='timeDesc'>Time (Newest)</option>
-                            <option value='timeAsc'>Time (Oldest)</option>
-                            <option value='idAsc'>Node ID (Increasing)</option>
-                            <option value='idDesc'>Node ID (Decreasing)</option>
-                        </Form.Select>
-                    </InputGroup>
+                    <div className='packet-sort'>
+                        <h5>Order</h5>
+                        <InputGroup className="mb-3" size='sm'>
+                            <InputGroup.Text id="basic-addon1">Sort by:</InputGroup.Text>
+                            <Form.Select size='sm'>
+                                <option value={Sort.timeDesc}>Time (Newest)</option>
+                                <option value={Sort.timeAsc}>Time (Oldest)</option>
+                                <option value={Sort.idAsc}>Node ID (Increasing)</option>
+                                <option value={Sort.idDesc}>Node ID (Decreasing)</option>
+                            </Form.Select>
+                        </InputGroup>
+                    </div>
                     <br />
-                    <h5>Filters</h5>
-                    <Row>
-                        <Col>
-                            <InputGroup className='mb-3' size='sm'>
-                                <InputGroup.Text id='basic-addon2'>After:</InputGroup.Text>
-                                <Form.Control size='sm' placeholder={'11T082554602'}/>
-                            </InputGroup>
-                        </Col>
-                        <Col>
-                            <InputGroup className='mb-3' size='sm'>
-                                <InputGroup.Text id='basic-addon3'>Before:</InputGroup.Text>
-                                <Form.Control size='sm' placeholder={'11T082620711'}/>
-                            </InputGroup>
-                        </Col>
-                    </Row>
-                    <InputGroup className='mb-3' size='sm'>
-                        <InputGroup.Text id='basic-addon4'>Node:</InputGroup.Text>
-                        <Form.Control size='sm' placeholder={'18F0010B'}/>
-                    </InputGroup>
+                    <div className='packet-filter'>
+                        <h5>Filters</h5>
+                        <Row>
+                            <Col>
+                                <InputGroup className='mb-3' size='sm'>
+                                    <InputGroup.Text id='basic-addon2'>After:</InputGroup.Text>
+                                    <Form.Control size='sm' placeholder={'11T082554602'}/>
+                                </InputGroup>
+                            </Col>
+                            <Col>
+                                <InputGroup className='mb-3' size='sm'>
+                                    <InputGroup.Text id='basic-addon3'>Before:</InputGroup.Text>
+                                    <Form.Control size='sm' placeholder={'11T082620711'}/>
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <InputGroup className='mb-3' size='sm'>
+                            <InputGroup.Text id='basic-addon4'>Node:</InputGroup.Text>
+                            <Form.Control size='sm' placeholder={'18F0010B'}/>
+                        </InputGroup>
+                    </div>
                     <br />
                     <Button variant="primary" size='sm' className='rounded-pill'>
                         Reset
--- src/visualizer/index.tsx
+++ src/visualizer/index.tsx
@@ -1,12 +1,33 @@
 import {useParams} from 'react-router-dom'
+import { useState } from 'react'
 import PacketContainer from './packetContainer'
+import PacketViewSettingsModal from './PacketViewSettingsModal'
 import './index.css'
 
 function Visualizer() {
     const params = useParams()
+
+    // Modal for changing packet view settings
+    let [isShownPacketsModal, setIsShownPacketsModal] = useState(true)
+
+    const cancelPacketModalSettings = () => {
+        setIsShownPacketsModal(false)
+    }
+
+    const applyPacketModalSettings = (e: any) => {
+        e.preventDefault()
+        setIsShownPacketsModal(false)
+    }
+
+    // Other stuff
     
     return (
         <div className='visualizer'>
+            <PacketViewSettingsModal
+                show={isShownPacketsModal}
+                onHide={cancelPacketModalSettings}
+                onSubmit={applyPacketModalSettings}
+            />
             <h1 className='visualizer-title'>{params.projectId}</h1>
             <PacketContainer></PacketContainer>
         </div>
