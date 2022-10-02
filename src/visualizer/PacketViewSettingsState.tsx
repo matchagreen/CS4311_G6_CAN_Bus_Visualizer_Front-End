95bc9168be7c88be8690c4c3b709df8ff188f475
christiancordova107 <cjcordova@miners.utep.edu>
2022-10-02 14:19:12 -0600

    change files location

--- src/visualizer/PacketViewSettingsState.tsx
+++
@@ -1,9 +0,0 @@
-interface PacketViewSettingsState {
-    size: number,
-    before: string | undefined,
-    after: string | undefined,
-    node: string | undefined,
-    sort: string
-}
-
-export default PacketViewSettingsState
